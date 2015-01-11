f5213e426e4c71eac980c9404d19403c9c998e05
Andrew Wilkinson <AndyWilkinsonNews@gmail.com>
2015-01-11 21:05:13 +0000

    Add preliminary data models with hardcoded sample data

---
+++ Okra-Todo/Data/TodoItem.cs
@@ -0,0 +1,39 @@
+﻿using Okra.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Okra_Todo.Data
+{
+    public class TodoItem : NotifyPropertyChangedBase
+    {
+        private string title;
+        private bool completed;
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                SetProperty(ref title, value);
+            }
+        }
+
+        public bool Completed
+        {
+            get
+            {
+                return completed;
+            }
+            set
+            {
+                SetProperty(ref completed, value);
+            }
+        }
+    }
+}
---
+++ Okra-Todo/Data/TodoRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Okra_Todo.Data
+{
+    [Export(typeof(ITodoRepository))]
+    public class TodoRepository
+    {
+        public IList<TodoItem> GetTodoItems()
+        {
+            return new List<TodoItem>
+                {
+                    new TodoItem() { Title = "First item"},
+                    new TodoItem() { Title = "Second item"},
+                    new TodoItem() { Title = "Third item"}
+                };
+        }
+    }
+}
