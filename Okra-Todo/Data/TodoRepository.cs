66212a22b0506f357817007f13a7b1e194b13de8
Andrew Wilkinson <AndyWilkinsonNews@gmail.com>
2015-01-12 22:01:22 +0000

    Refactor TodoRepository and include Add and Remove methods

--- Okra-Todo/Data/TodoRepository.cs
+++ Okra-Todo/Data/TodoRepository.cs
@@ -10,19 +10,36 @@ namespace Okra.TodoSample.Data
     [Export(typeof(ITodoRepository))]
     public class TodoRepository : ITodoRepository
     {
-        public TodoItem GetTodoItemById(string id)
-        {
-            return GetTodoItems().Where(item => item.Id == id).FirstOrDefault();
-        }
+        private IList<TodoItem> todoItems;
 
-        public IList<TodoItem> GetTodoItems()
+        public TodoRepository()
         {
-            return new List<TodoItem>
+            this.todoItems = new List<TodoItem>
                 {
                     new TodoItem() { Id = "1", Title = "First item"},
                     new TodoItem() { Id = "2", Title = "Second item"},
                     new TodoItem() { Id = "3", Title = "Third item"}
                 };
+        }
+
+        public TodoItem GetTodoItemById(string id)
+        {
+            return todoItems.Where(item => item.Id == id).FirstOrDefault();
+        }
+
+        public IList<TodoItem> GetTodoItems()
+        {
+            return todoItems;
+        }
+
+        public void AddTodoItem(TodoItem todoItem)
+        {
+            this.todoItems.Add(todoItem);
+        }
+
+        public void RemoveTodoItem(TodoItem todoItem)
+        {
+            this.todoItems.Remove(todoItem);
         }
     }
 }
