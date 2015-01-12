f8025b4b990a2eb76716ff0f119326ca94c11d3e
Andrew Wilkinson <AndyWilkinsonNews@gmail.com>
2015-01-12 19:31:30 +0000

    Add 'ItemDetail' page

---
+++ Okra-Todo/Pages/ItemDetail/ItemDetailDesignData.cs
@@ -0,0 +1,17 @@
+﻿using Okra.TodoSample.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Okra.TodoSample.Pages.ItemDetail
+{
+    public class ItemDetailDesignData : ItemDetailViewModel
+    {
+        public ItemDetailDesignData()
+        {
+            this.TodoItem = new TodoItemDataModel { Title = "My Todo Item" };
+        }
+    }
+}
---
+++ Okra-Todo/Pages/ItemDetail/ItemDetailViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Okra.TodoSample.Common;
+using Okra.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Okra.TodoSample.DataModels;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace Okra.TodoSample.Pages.ItemDetail
+{
+    /// <summary>
+    /// A basic view model that provides characteristics common to most applications.
+    /// </summary>
+    [ViewModelExport("ItemDetail")]
+    public class ItemDetailViewModel : ViewModelBase
+    {
+        private TodoItemDataModel todoItem;
+
+        [ImportingConstructor]
+        public ItemDetailViewModel(INavigationContext navigationContext)
+            : base(navigationContext)
+        {
+        }
+
+        protected ItemDetailViewModel()
+        {
+        }
+
+        public TodoItemDataModel TodoItem
+        {
+            get
+            {
+                return todoItem;
+            }
+            set
+            {
+                SetProperty(ref todoItem, value);
+            }
+        }
+    }
+}
