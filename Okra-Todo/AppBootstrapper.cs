1a249502c9798ab21439a59525155b34d3f22bbf
Andrew Wilkinson <AndyWilkinsonNews@gmail.com>
2015-01-25 20:33:10 +0000

    Add "About" settings flyout

--- Okra-Todo/AppBootstrapper.cs
+++ Okra-Todo/AppBootstrapper.cs
@@ -6,11 +6,16 @@ using System.Composition;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources;
+using Windows.UI.ApplicationSettings;
 
 namespace Okra.TodoSample
 {
     public class AppBootstrapper : OkraBootstrapper
     {
+        [Import]
+        public ISettingsPaneManager SettingsPaneManager { get; set; }
+
         /// <summary>
         /// Perform general initialization of application services.
         /// </summary>
@@ -18,6 +23,21 @@ namespace Okra.TodoSample
         {
             // Configure the navigation manager to preseve application state in local storage
             NavigationManager.NavigationStorageType = NavigationStorageType.Local;
+
+            // Register with Windows to display items in the settings pane
+            SettingsPane.GetForCurrentView().CommandsRequested += SettingsPane_CommandsRequested;
+        }
+
+        void SettingsPane_CommandsRequested(SettingsPane sender, SettingsPaneCommandsRequestedEventArgs args)
+        {
+            // The settings names should be localised so create a ResourceLoader to gather these from
+
+            ResourceLoader resourceLoader = new ResourceLoader();
+
+            // Add each settings item in the order you wish them to appear
+            // NB: Since we want the SettingsPaneManager to navigate when the item is selected we use the GetNavigateToSettingsCommand helper
+
+            args.Request.ApplicationCommands.Add(SettingsPaneManager.GetNavigateToSettingsCommand(resourceLoader.GetString("AboutCommandLabel"), "AboutPage"));
         }
     }
 }
---
+++ Okra-Todo/Pages/About/AboutViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Okra.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Okra.TodoSample.Common;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel;
+
+// The Settings Flyout item template is documented at http://go.microsoft.com/fwlink/?LinkId=273769
+
+namespace Okra.TodoSample.Pages.About
+{
+    /// <summary>
+    /// A view model for displaying an overview of a single group, including a preview of the items
+    /// within the group.
+    /// </summary>
+    [ViewModelExport("AboutPage")]
+    public class AboutViewModel : ViewModelBase
+    {
+        [ImportingConstructor]
+        public AboutViewModel(INavigationContext navigationContext)
+            : base(navigationContext)
+        {
+            PackageVersion version = Package.Current.Id.Version;
+            this.ApplicationVersion = string.Format("{0}.{1}.{2}", version.Major, version.Minor, version.Revision);
+        }
+
+        public string ApplicationVersion
+        {
+            get;
+            private set;
+        }
+    }
+}
